1ac245e490d09f10101a1f4f529afd3ad119c305
Barak Ugav <barakugav@gmail.com>
2022-06-05 01:13:53 +0300

    Directed Linked Graph: specific implementation, support fast in/out edges iterate/remove

---
+++ src/main/java/com/ugav/algo/GraphLinkedAbstract.java
@@ -0,0 +1,93 @@
+package com.ugav.algo;
+
+import java.util.AbstractCollection;
+import java.util.Collection;
+
+abstract class GraphLinkedAbstract<E> extends GraphAbstract<E> {
+
+	int n;
+	int m;
+	Collection<Edge<E>> edgesView;
+
+	GraphLinkedAbstract(int n) {
+		if (n < 0)
+			throw new IllegalArgumentException();
+		this.n = n;
+		m = 0;
+	}
+
+	@Override
+	public int vertices() {
+		return n;
+	}
+
+	@Override
+	public int newVertex() {
+		return n++;
+	}
+
+	@Override
+	public Collection<Edge<E>> edges() {
+		return edgesView;
+	}
+
+	@Override
+	public void addEdge(Edge<E> e) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		edges().clear();
+		n = 0;
+	}
+
+	void checkVertexIdentifier(int v) {
+		if (!(0 <= v && v < n))
+			throw new IllegalArgumentException("Illegal vertex identifier");
+	}
+
+	abstract static class NodeAbstact<E> extends EdgeAbstract<E> {
+
+		final int u;
+		final int v;
+
+		NodeAbstact(int u, int v) {
+			this.u = u;
+			this.v = v;
+		}
+
+		@Override
+		public int u() {
+			return u;
+		}
+
+		@Override
+		public int v() {
+			return v;
+		}
+
+	}
+
+	abstract class EdgesViewAbstract extends AbstractCollection<Edge<E>> {
+
+		@Override
+		public int size() {
+			return m;
+		}
+
+		@Override
+		public boolean add(Edge<E> e) {
+			throw new UnsupportedOperationException();
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public boolean remove(Object o) {
+			removeEdge((Edge<E>) o);
+			return true;
+		}
+
+	}
+
+}
---
+++ src/main/java/com/ugav/algo/GraphLinkedDirected.java
@@ -0,0 +1,305 @@
+package com.ugav.algo;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class GraphLinkedDirected<E> extends GraphLinkedAbstract<E> {
+
+	private Node<E>[] edgesIn;
+	private Node<E>[] edgesOut;
+
+	public GraphLinkedDirected() {
+		this(0);
+	}
+
+	@SuppressWarnings("unchecked")
+	public GraphLinkedDirected(int n) {
+		super(n);
+		edgesIn = new Node[n != 0 ? n : 1];
+		edgesOut = new Node[n != 0 ? n : 1];
+		edgesView = new EdgesView();
+	}
+
+	@Override
+	public int newVertex() {
+		int v = super.newVertex();
+		if (edgesIn.length < n) {
+			edgesIn = Arrays.copyOf(edgesIn, edgesIn.length * 2);
+			edgesOut = Arrays.copyOf(edgesOut, edgesOut.length * 2);
+		}
+		return v;
+	}
+
+	@Override
+	public Iterator<Edge<E>> edges(int u) {
+		return edgesOut(u);
+	}
+
+	public Iterator<Edge<E>> edgesOut(int u) {
+		checkVertexIdentifier(u);
+		return new EdgeVertexItrOut<>(edgesOut[u]);
+	}
+
+	public Iterator<Edge<E>> edgesIn(int v) {
+		checkVertexIdentifier(v);
+		return new EdgeVertexItrIn<>(edgesIn[v]);
+	}
+
+	@Override
+	public Edge<E> getEdge(int u, int v) {
+		checkVertexIdentifier(u);
+		checkVertexIdentifier(v);
+		for (Node<E> n = edgesOut[u]; n != null; n = n.nextOut)
+			if (n.v == v)
+				return n;
+		return null;
+	}
+
+	@Override
+	public Edge<E> addEdge(int u, int v) {
+		checkVertexIdentifier(u);
+		checkVertexIdentifier(v);
+
+		Node<E> e = new Node<>(u, v), next;
+		next = edgesOut[u];
+		if (next != null) {
+			next.prevOut = e;
+			e.nextOut = next;
+		}
+		edgesOut[u] = e;
+		next = edgesIn[v];
+		if (next != null) {
+			next.prevOut = e;
+			e.nextIn = next;
+		}
+		edgesIn[v] = e;
+
+		m++;
+		return e;
+	}
+
+	@Override
+	public void addEdge(Edge<E> e) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void removeEdge(Edge<E> e) {
+		removeEdgeOut((Node<E>) e);
+		removeEdgeIn((Node<E>) e);
+		m--;
+	}
+
+	@Override
+	public void removeEdgesOut(int u) {
+		int count = 0;
+		for (Node<E> p = edgesOut[u], next; p != null; p = next) {
+			next = p.nextOut;
+			p.nextOut = p.prevOut = null;
+			removeEdgeIn(p);
+			count++;
+		}
+		edgesOut[u] = null;
+		m -= count;
+	}
+
+	public void removeEdgesIn(int v) {
+		int count = 0;
+		for (Node<E> p = edgesIn[v], next; p != null; p = next) {
+			next = p.nextIn;
+			p.nextIn = p.prevIn = null;
+			removeEdgeOut(p);
+			count++;
+		}
+		edgesIn[v] = null;
+		m -= count;
+	}
+
+	private void removeEdgeOut(Node<E> e) {
+		Node<E> next = e.nextOut, prev = e.prevOut;
+		if (prev == null) {
+			edgesOut[e.u] = next;
+		} else {
+			prev.nextOut = next;
+			e.prevOut = null;
+		}
+		if (next != null) {
+			next.prevOut = prev;
+			e.nextOut = null;
+		}
+	}
+
+	private void removeEdgeIn(Node<E> e) {
+		Node<E> next = e.nextIn, prev = e.prevIn;
+		if (prev == null) {
+			edgesIn[e.u] = next;
+		} else {
+			prev.nextIn = next;
+			e.prevIn = null;
+		}
+		if (next != null) {
+			next.prevIn = prev;
+			e.nextIn = null;
+		}
+	}
+
+	@Override
+	public boolean isDirected() {
+		return true;
+	}
+
+	private static class Node<E> extends NodeAbstact<E> {
+
+		private Node<E> nextOut;
+		private Node<E> nextIn;
+		private Node<E> prevOut;
+		private Node<E> prevIn;
+
+		private Node(int u, int v) {
+			super(u, v);
+		}
+
+		@Override
+		public Edge<E> twin() {
+			return null;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o == this)
+				return true;
+			if (!(o instanceof Edge))
+				return false;
+
+			Edge<?> e = (Edge<?>) o;
+			return u == e.u() && v == e.v() && Objects.equals(val(), e.val());
+		}
+
+		@Override
+		public int hashCode() {
+			return u ^ ~v ^ Objects.hashCode(val());
+		}
+
+	}
+
+	private static abstract class EdgeVertexItr<E> implements Iterator<Edge<E>> {
+
+		Node<E> p;
+
+		EdgeVertexItr(Node<E> p) {
+			this.p = p;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return p != null;
+		}
+
+	}
+
+	private static class EdgeVertexItrOut<E> extends EdgeVertexItr<E> {
+
+		EdgeVertexItrOut(Node<E> p) {
+			super(p);
+		}
+
+		@Override
+		public Edge<E> next() {
+			if (!hasNext())
+				throw new NoSuchElementException();
+			Node<E> q = p;
+			p = q.nextOut;
+			return q;
+		}
+
+	}
+
+	private static class EdgeVertexItrIn<E> extends EdgeVertexItr<E> {
+
+		EdgeVertexItrIn(Node<E> p) {
+			super(p);
+		}
+
+		@Override
+		public Edge<E> next() {
+			if (!hasNext())
+				throw new NoSuchElementException();
+			Node<E> q = p;
+			p = q.nextIn;
+			return q;
+		}
+
+	}
+
+	private class EdgesItr implements Iterator<Edge<E>> {
+
+		int idx;
+		Node<E> p;
+
+		EdgesItr() {
+			idx = 0;
+			p = edgesOut[0];
+		}
+
+		@Override
+		public boolean hasNext() {
+			for (;;) {
+				if (p != null)
+					return true;
+				if (++idx >= n)
+					return false;
+				p = edgesOut[idx];
+			}
+		}
+
+		@Override
+		public Edge<E> next() {
+			if (!hasNext())
+				throw new NoSuchElementException();
+			Node<E> q = p;
+			p = q.nextOut;
+			return q;
+		}
+
+	}
+
+	private class EdgesView extends EdgesViewAbstract {
+
+		@Override
+		public boolean contains(Object o) {
+			if (!(o instanceof Edge<?>))
+				return false;
+			Edge<?> e = (Edge<?>) o;
+			int u = e.u();
+			if (u >= n || e.v() >= n)
+				return false;
+			for (Node<E> n = edgesOut[u]; n != null; n = n.nextOut)
+				if (n == e)
+					return true;
+			return false;
+		}
+
+		@Override
+		public Iterator<Edge<E>> iterator() {
+			return new EdgesItr();
+		}
+
+		@Override
+		public void clear() {
+			Node<E>[] es = edgesOut;
+			for (int i = 0; i < es.length; i++) {
+				for (Node<E> p = es[i], next; p != null; p = next) {
+					next = p.nextOut;
+					p.nextOut = p.prevOut = p.nextIn = p.prevIn = null;
+				}
+				es[i] = null;
+			}
+			Arrays.fill(edgesIn, null);
+			m = 0;
+		}
+
+	}
+
+}
