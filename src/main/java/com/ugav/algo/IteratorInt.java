b2eb38fe6573320c2075bc7c15d60b3dcef25ec5
Barak Ugav <barakugav@gmail.com>
2022-06-08 19:00:20 +0300

    Linked lists implementations

---
+++ src/main/java/com/ugav/algo/IteratorInt.java
@@ -0,0 +1,26 @@
+package com.ugav.algo;
+
+import java.util.Iterator;
+
+interface IteratorInt {
+
+	boolean hasNext();
+
+	int next();
+
+	default Iterator<Integer> asIteratorObj() {
+		return new Iterator<>() {
+
+			@Override
+			public boolean hasNext() {
+				return IteratorInt.this.hasNext();
+			}
+
+			@Override
+			public Integer next() {
+				return Integer.valueOf(IteratorInt.this.next());
+			}
+		};
+	}
+
+}
