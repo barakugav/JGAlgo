4f8953d7d32aef37858f197a37423c183fea5d01
Barak Ugav <barakugav@gmail.com>
2023-04-07 14:35:29 +0300

    Connectivity: tests

--- src/test/java/com/jgalgo/test/Utils.java
+++ src/test/java/com/jgalgo/test/Utils.java
@@ -1,6 +1,10 @@
 package com.jgalgo.test;
 
+import java.util.BitSet;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import it.unimi.dsi.fastutil.ints.IntIterator;
 
 class Utils {
 	private Utils() {
@@ -13,6 +17,27 @@ class Utils {
 			@Override
 			public Iterator<E> iterator() {
 				return it;
+			}
+		};
+	}
+
+	static IntIterator bitSetIterator(BitSet bitSet) {
+		return new IntIterator() {
+
+			int bit = bitSet.nextSetBit(0);
+
+			@Override
+			public boolean hasNext() {
+				return bit != -1;
+			}
+
+			@Override
+			public int nextInt() {
+				if (!hasNext())
+					throw new NoSuchElementException();
+				int ret = bit;
+				bit = bitSet.nextSetBit(bit + 1);
+				return ret;
 			}
 		};
 	}
