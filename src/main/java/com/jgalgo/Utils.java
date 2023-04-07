62e76b8b53f234c31fcb9db4b402f746267ca5f3
Barak Ugav <barakugav@gmail.com>
2023-04-07 17:43:45 +0300

    Utils: remove ArrayView

--- src/main/java/com/jgalgo/Utils.java
+++ src/main/java/com/jgalgo/Utils.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.RandomAccess;
 
 import it.unimi.dsi.fastutil.ints.IntIterator;
 
@@ -270,88 +269,6 @@ class Utils {
 			}
 		}
 
-	}
-
-	static class ArrayView<E> extends AbstractList<E> implements RandomAccess {
-
-		private final E[] a;
-		private int size;
-
-		ArrayView(E[] array, int size) {
-			a = Objects.requireNonNull(array);
-			this.size = size;
-		}
-
-		public void setSize(int size) {
-			if (size < 0 || size > a.length)
-				throw new IllegalArgumentException();
-			this.size = size;
-		}
-
-		@Override
-		public int size() {
-			return size;
-		}
-
-		@Override
-		public E get(int index) {
-			if (index >= size)
-				throw new IndexOutOfBoundsException("index " + index + " out of bound for size " + size);
-			return a[index];
-		}
-
-		@Override
-		public E set(int index, E element) {
-			if (index >= size)
-				throw new IndexOutOfBoundsException("index " + index + " out of bound for size " + size);
-			E oldValue = a[index];
-			a[index] = element;
-			return oldValue;
-		}
-
-		@Override
-		public int indexOf(Object o) {
-			E[] a = this.a;
-			if (o == null) {
-				for (int i = 0; i < size; i++)
-					if (a[i] == null)
-						return i;
-			} else {
-				for (int i = 0; i < size; i++)
-					if (o.equals(a[i]))
-						return i;
-			}
-			return -1;
-		}
-
-		@Override
-		public boolean contains(Object o) {
-			return indexOf(o) >= 0;
-		}
-
-		@Override
-		public Iterator<E> iterator() {
-			return new ArrayItr();
-		}
-
-		private class ArrayItr implements Iterator<E> {
-			private int cursor;
-
-			@Override
-			public boolean hasNext() {
-				return cursor < size;
-			}
-
-			@Override
-			public E next() {
-				int i = cursor;
-				if (i >= size) {
-					throw new NoSuchElementException();
-				}
-				cursor = i + 1;
-				return a[i];
-			}
-		}
 	}
 
 	static class NullIterator<E> implements Iterator<E> {
