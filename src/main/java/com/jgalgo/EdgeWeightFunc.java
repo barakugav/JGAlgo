de8d64a116ef31c8b42b04304f817d14c50b2cbe
Barak Ugav <barakugav@gmail.com>
2023-04-17 22:53:51 +0300

    EdgeWeightFunc: documentation

--- src/main/java/com/jgalgo/EdgeWeightFunc.java
+++ src/main/java/com/jgalgo/EdgeWeightFunc.java
@@ -2,18 +2,119 @@ package com.jgalgo;
 
 import it.unimi.dsi.fastutil.ints.IntComparator;
 
+/**
+ * Weight function that maps a graph edge to a weight.
+ * <p>
+ * Many algorithms such as {@link SSSP}, {@link MST}, {@link MatchingWeighted},
+ * and more, try to find a set of edges satisfying some constraint while
+ * minimizing/maximizing some objective function based on the weights of the
+ * edges. This interface is the API by which the user specify the weights of the
+ * edges.
+ * <p>
+ *
+ * <pre> {@code
+ * // Create a directed graph with three vertices and edges between them
+ * DiGraph g = new GraphArrayDirected();
+ * int v1 = g.addVertex();
+ * int v2 = g.addVertex();
+ * int v3 = g.addVertex();
+ * int e1 = g.addEdge(v1, v2);
+ * int e2 = g.addEdge(v2, v3);
+ * int e3 = g.addEdge(v1, v3);
+ *
+ * // Assign some weights to the edges
+ * Weights.Double weights = g.addEdgesWeights("weightsKey", double.class);
+ * weights.set(e1, 1.2);
+ * weights.set(e2, 3.1);
+ * weights.set(e3, 15.1);
+ * EdgeWeightFunc weightFunc = weights;
+ *
+ * // Calculate the shortest paths from v1 to all other vertices
+ * SSSP ssspAlgo = new SSSPDijkstra();
+ * SSSP.Result ssspRes = ssspAlgo.calcDistances(g, weightFunc, v1);
+ *
+ * // Print the shortest path from v1 to v3
+ * assert ssspRes.distance(v3) == 4.3;
+ * assert ssspRes.getPathTo(v3).equals(IntList.of(e1, e2));
+ * System.out.println("Distance from v1 to v3 is: " + ssspRes.distance(v3));
+ * System.out.println("The shortest path from v1 to v3 is:");
+ * for (IntIterator it = ssspRes.getPathTo(v3).iterator(); it.hasNext();) {
+ * 	int e = it.nextInt();
+ * 	int u = g.edgeSource(e), v = g.edgeTarget(e);
+ * 	System.out.println(" " + e + "(" + u + ", " + v + ")");
+ * }
+ * }</pre>
+ *
+ * @author Barak Ugav
+ */
 @FunctionalInterface
 public interface EdgeWeightFunc extends IntComparator {
 
+	/**
+	 * Get the weight of an edge.
+	 *
+	 * @param e an edge identifier
+	 * @return the weight of the edge
+	 * @throws IndexOutOfBoundsException if {@code edge} is not a valid edge
+	 *                                   identifier
+	 */
 	public double weight(int e);
 
+	/**
+	 * Compare two edges by their weights.
+	 */
 	@Override
 	default int compare(int e1, int e2) {
 		return Double.compare(weight(e1), weight(e2));
 	}
 
+	/**
+	 * Weight function that maps a graph edge to an integer weight.
+	 * <p>
+	 * Some algorithms implementations support only integers weights, or run faster
+	 * in such a case. This interface is the API for these algorithms for the edges
+	 * integer weights.
+	 * <p>
+	 *
+	 * <pre> {@code
+	 * // Create a directed graph with three vertices and edges between them
+	 * DiGraph g = new GraphArrayDirected();
+	 * int v1 = g.addVertex();
+	 * int v2 = g.addVertex();
+	 * int v3 = g.addVertex();
+	 * int e1 = g.addEdge(v1, v2);
+	 * int e2 = g.addEdge(v2, v3);
+	 * int e3 = g.addEdge(v1, v3);
+	 *
+	 * // Assign some weights to the edges
+	 * Weights.Int weights = g.addEdgesWeights("weightsKey", int.class);
+	 * weights.set(e1, 1);
+	 * weights.set(e2, 3);
+	 * weights.set(e3, 15);
+	 * EdgeWeightFunc.Int weightFunc = weights;
+	 *
+	 * // Calculate the shortest paths from v1 to all other vertices
+	 * SSSP ssspAlgo = new SSSPDial1969();
+	 * SSSP.Result ssspRes = ssspAlgo.calcDistances(g, weightFunc, v1);
+	 *
+	 * // Print the shortest path from v1 to v3
+	 * assert ssspRes.distance(v3) == 4;
+	 * assert ssspRes.getPathTo(v3).equals(IntList.of(e1, e2));
+	 * System.out.println("Distance from v1 to v3 is: " + ssspRes.distance(v3));
+	 * System.out.println("The shortest path from v1 to v3 is:");
+	 * for (IntIterator it = ssspRes.getPathTo(v3).iterator(); it.hasNext();) {
+	 * 	int e = it.nextInt();
+	 * 	int u = g.edgeSource(e), v = g.edgeTarget(e);
+	 * 	System.out.println(" " + e + "(" + u + ", " + v + ")");
+	 * }
+	 * }</pre>
+	 *
+	 * @see SSSPDial1969
+	 * @see SSSPGoldberg1995
+	 * @author Barak Ugav
+	 */
 	@FunctionalInterface
-	public static interface Int extends EdgeWeightFunc, IntComparator {
+	public static interface Int extends EdgeWeightFunc {
 
 		@Deprecated
 		@Override
@@ -21,6 +122,14 @@ public interface EdgeWeightFunc extends IntComparator {
 			return weightInt(e);
 		}
 
+		/**
+		 * Get the integer weight of an edge.
+		 *
+		 * @param e an edge identifier
+		 * @return the integer weight of the edge
+		 * @throws IndexOutOfBoundsException if {@code edge} is not a valid edge
+		 *                                   identifier
+		 */
 		public int weightInt(int e);
 
 		@Override
