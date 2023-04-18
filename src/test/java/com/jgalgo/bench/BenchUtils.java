09e3d0ca036e67d22821ad5244ad5acbf1c63568
Barak Ugav <barakugav@gmail.com>
2023-04-18 08:48:58 +0300

    Formatting: use tabs

--- src/test/java/com/jgalgo/bench/BenchUtils.java
+++ src/test/java/com/jgalgo/bench/BenchUtils.java
@@ -6,16 +6,16 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
 
 public class BenchUtils {
 
-    static Map<String, String> parseArgsStr(String s) {
-        String[] strs = s.split(" ");
-        Map<String, String> args = new Object2ObjectArrayMap<>(strs.length);
-        for (String arg : strs) {
-            int idx = arg.indexOf('=');
-            String key = arg.substring(0, idx);
-            String value = arg.substring(idx + 1);
-            args.put(key, value);
-        }
-        return args;
-    }
+	static Map<String, String> parseArgsStr(String s) {
+		String[] strs = s.split(" ");
+		Map<String, String> args = new Object2ObjectArrayMap<>(strs.length);
+		for (String arg : strs) {
+			int idx = arg.indexOf('=');
+			String key = arg.substring(0, idx);
+			String value = arg.substring(idx + 1);
+			args.put(key, value);
+		}
+		return args;
+	}
 
 }
