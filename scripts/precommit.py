72dc790f2d43552d5f856f7a43bb0648f2aa04e5
Barak Ugav <barakugav@gmail.com>
2023-04-25 15:49:56 +0300

    Sripts: few args in precommint script

--- scripts/precommit.py
+++ scripts/precommit.py
@@ -1,31 +1,48 @@
 import os
 import subprocess
+import argparse
+
 
 SCRIPTS_DIR = os.path.dirname(os.path.realpath(__file__))
 TOP_DOR = os.path.abspath(os.path.join(SCRIPTS_DIR, ".."))
 
 
-def main():
+def main(args):
     def run_cmd(cmd):
         subprocess.check_call(cmd, cwd=TOP_DOR, shell=True)
 
-    print("\n\n ============ Clean ============\n")
-    run_cmd("mvn clean")
-
-    print("\n\n ============ Build ============\n")
-    run_cmd("mvn --batch-mode --update-snapshots package -Dmaven.test.skip")
+    if not args.skip_rebuild:
+        print("\n\n ============ Clean ============\n")
+        run_cmd("mvn clean")
+        print("\n\n ============ Build ============\n")
+        run_cmd("mvn --batch-mode --update-snapshots package -Dmaven.test.skip")
 
     print("\n\n ============ Tests ============\n")
-    run_cmd("mvn --batch-mode test -DfailIfNoTests=false")
+    if not args.skip_tests:
+        run_cmd("mvn --batch-mode test -DfailIfNoTests=false")
+    else:
+        print("skipping...")
 
     print("\n\n ============ SpotBugs ============\n")
-    run_cmd("mvn --batch-mode spotbugs:check -pl jgalgo-core")
+    if not args.skip_static:
+        run_cmd("mvn --batch-mode spotbugs:check -pl jgalgo-core")
+    else:
+        print("skipping...")
 
     print("\n\n ============ Javadoc ============\n")
-    run_cmd("mvn javadoc:aggregate -pl jgalgo-core")
+    if not args.skip_javadoc:
+        run_cmd("mvn javadoc:aggregate -pl jgalgo-core")
+    else:
+        print("skipping...")
 
     print("\nPre-commit check passed successfully")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(prog='ProgramName')
+    parser.add_argument('--skip-rebuild', action='store_true',  help='skip clean and build')
+    parser.add_argument('--skip-tests', action='store_true',  help='skip all unit tests')
+    parser.add_argument('--skip-static', action='store_true',  help='skip static analysis')
+    parser.add_argument('--skip-javadoc', action='store_true',  help='skip Javadoc generation')
+    args = parser.parse_args()
+    main(args)
