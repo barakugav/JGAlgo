3697d3de803e9786a863b25fa277a7797af53d44
Barak Ugav <barakugav@gmail.com>
2023-08-04 14:42:15 +0300

    Python: use the 'black' formatter

--- scripts/precommit.py
+++ scripts/precommit.py
@@ -8,42 +8,48 @@ TOP_DOR = os.path.abspath(os.path.join(SCRIPTS_DIR, ".."))
 
 
 def main(args):
-	def run_cmd(cmd):
-		subprocess.check_call(cmd, cwd=TOP_DOR, shell=True)
+    def run_cmd(cmd):
+        subprocess.check_call(cmd, cwd=TOP_DOR, shell=True)
 
-	print("\n\n ============ Clean and Build ============\n")
-	if not args.skip_rebuild:
-		run_cmd("mvn clean")
-		run_cmd("mvn --batch-mode --update-snapshots package -Dmaven.test.skip")
-	else:
-		print("skipping...")
+    print("\n\n ============ Clean and Build ============\n")
+    if not args.skip_rebuild:
+        run_cmd("mvn clean")
+        run_cmd("mvn --batch-mode --update-snapshots package -Dmaven.test.skip")
+    else:
+        print("skipping...")
 
-	print("\n\n ============ Tests ============\n")
-	if not args.skip_tests:
-		run_cmd("mvn --batch-mode test -DfailIfNoTests=false")
-	else:
-		print("skipping...")
+    print("\n\n ============ Tests ============\n")
+    if not args.skip_tests:
+        run_cmd("mvn --batch-mode test -DfailIfNoTests=false")
+    else:
+        print("skipping...")
 
-	print("\n\n ============ SpotBugs ============\n")
-	if not args.skip_static:
-		run_cmd("mvn --batch-mode spotbugs:check -pl jgalgo-core")
-	else:
-		print("skipping...")
+    print("\n\n ============ SpotBugs ============\n")
+    if not args.skip_static:
+        run_cmd("mvn --batch-mode spotbugs:check -pl jgalgo-core")
+    else:
+        print("skipping...")
 
-	print("\n\n ============ Javadoc ============\n")
-	if not args.skip_javadoc:
-		run_cmd("mvn javadoc:aggregate")
-	else:
-		print("skipping...")
+    print("\n\n ============ Javadoc ============\n")
+    if not args.skip_javadoc:
+        run_cmd("mvn javadoc:aggregate")
+    else:
+        print("skipping...")
 
-	print("\nPre-commit check passed successfully")
+    print("\nPre-commit check passed successfully")
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(prog='ProgramName')
-	parser.add_argument('--skip-rebuild', action='store_true', help='skip clean and build')
-	parser.add_argument('--skip-tests', action='store_true', help='skip all unit tests')
-	parser.add_argument('--skip-static', action='store_true', help='skip static analysis')
-	parser.add_argument('--skip-javadoc', action='store_true', help='skip Javadoc generation')
-	args = parser.parse_args()
-	main(args)
+    parser = argparse.ArgumentParser(prog="ProgramName")
+    parser.add_argument(
+        "--skip-rebuild", action="store_true", help="skip clean and build"
+    )
+    parser.add_argument("--skip-tests", action="store_true", help="skip all unit tests")
+    parser.add_argument(
+        "--skip-static", action="store_true", help="skip static analysis"
+    )
+    parser.add_argument(
+        "--skip-javadoc", action="store_true", help="skip Javadoc generation"
+    )
+    args = parser.parse_args()
+    main(args)
