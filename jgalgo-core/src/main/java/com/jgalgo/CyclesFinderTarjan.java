774456cff9da72b8ae5f9b8294e5dc4751b03929
Barak Ugav <barakugav@gmail.com>
2023-08-06 20:14:06 +0300

    Cycle Finder Tarjan: implement as iterator, to avoid storing all cycles in memory

--- jgalgo-core/src/main/java/com/jgalgo/CyclesFinderTarjan.java
+++ jgalgo-core/src/main/java/com/jgalgo/CyclesFinderTarjan.java
@@ -18,12 +18,11 @@ package com.jgalgo;
 
 import java.util.BitSet;
 import java.util.Iterator;
-import java.util.List;
 import com.jgalgo.graph.EdgeIter;
 import com.jgalgo.graph.IndexGraph;
 import com.jgalgo.internal.util.Assertions;
+import it.unimi.dsi.fastutil.Stack;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.ints.IntStack;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 
 /**
@@ -46,70 +45,109 @@ class CyclesFinderTarjan extends CyclesFinderAbstract {
 	@Override
 	Iterator<Path> findAllCycles(IndexGraph g) {
 		Assertions.Graphs.onlyDirected(g);
-		Worker worker = new Worker(g);
-		int n = g.vertices().size();
-		for (int s = 0; s < n; s++) {
-			worker.findAllCycles(s);
-			worker.reset();
-		}
-		/* TODO: the intention of returning an iterator is to avoid storing all cycles in memory */
-		return worker.cycles.iterator();
-	}
+		return new Iterator<>() {
 
-	private static class Worker {
-		private final IndexGraph g;
-		private final IntStack path = new IntArrayList();
-		private final IntStack markedStack = new IntArrayList();
-		private final BitSet isMarked;
-		private final List<Path> cycles = new ObjectArrayList<>();
+			final int n = g.vertices().size();
+			int startV = 0;
+			final IntArrayList path = new IntArrayList();
+			final IntArrayList markedStack = new IntArrayList();
+			final BitSet isMarked = new BitSet(n);
+			final Stack<EdgeIter> edgeIterStack = new ObjectArrayList<>();
+			/**
+			 * In the paper, there is a boolean flag 'f' in each recursive call of the backtrack function. The flag is
+			 * set to true if a cycle was found in the current function call or its successors calls. This invariant
+			 * allow us to store a single int to represent all these flags, which is the deepest depth of the recursion
+			 * in which a cycle was found. For any depth smaller than this depth, the flag is also true. When we back up
+			 * to the store depth, we decrease it by one.
+			 */
+			int cycleFoundDepth = -1;
+			Path nextCycle;
 
-		Worker(IndexGraph g) {
-			this.g = g;
-			int n = g.vertices().size();
-			isMarked = new BitSet(n);
-		}
+			{
+				isMarked.set(startV);
+				markedStack.push(startV);
+				edgeIterStack.push(g.outEdges(startV).iterator());
 
-		void reset() {
-			((IntArrayList) path).clear();
-			((IntArrayList) markedStack).clear();
-			isMarked.clear();
-		}
-
-		boolean findAllCycles(int startV) {
-			boolean cycleFound = false;
-
-			int u = path.isEmpty() ? startV : g.edgeTarget(path.topInt());
-			isMarked.set(u);
-			markedStack.push(u);
-
-			for (EdgeIter it = g.outEdges(u).iterator(); it.hasNext();) {
-				int e = it.nextInt();
-				int v = it.target();
-				if (v < startV)
-					continue;
-				if (v == startV) {
-					path.push(e);
-					cycles.add(new PathImpl(g, startV, startV, new IntArrayList((IntArrayList) path)));
-					path.popInt();
-					cycleFound = true;
-				} else if (!isMarked.get(v)) {
-					path.push(e);
-					if (findAllCycles(startV))
-						cycleFound = true;
-					path.popInt();
-				}
-			}
-			if (cycleFound) {
-				while (markedStack.topInt() != u) {
-					int w = markedStack.popInt();
-					isMarked.clear(w);
-				}
-				markedStack.popInt();
-				isMarked.clear(u);
+				advance();
 			}
 
-			return cycleFound;
-		}
+			private void advance() {
+				if (startV >= n) {
+					nextCycle = null;
+					return;
+				}
+				for (;;) {
+					currentStartVLoop: while (!edgeIterStack.isEmpty()) {
+						for (EdgeIter it = edgeIterStack.top(); it.hasNext();) {
+							int e = it.nextInt();
+							int v = it.target();
+							if (v < startV)
+								continue;
+							if (v == startV) {
+								path.push(e);
+								nextCycle = new PathImpl(g, startV, startV, new IntArrayList(path));
+								path.popInt();
+								cycleFoundDepth = path.size();
+								return;
+
+							} else if (!isMarked.get(v)) {
+								path.push(e);
+								isMarked.set(v);
+								markedStack.push(v);
+								edgeIterStack.push(g.outEdges(v).iterator());
+								continue currentStartVLoop;
+							}
+						}
+
+						assert cycleFoundDepth <= path.size();
+						boolean cycleFound = cycleFoundDepth == path.size();
+
+						int u;
+						if (path.isEmpty()) {
+							u = startV;
+						} else {
+							u = g.edgeTarget(path.popInt());
+						}
+
+						if (cycleFound) {
+							while (markedStack.topInt() != u) {
+								int w = markedStack.popInt();
+								isMarked.clear(w);
+							}
+							markedStack.popInt();
+							isMarked.clear(u);
+							cycleFoundDepth--;
+						}
+						edgeIterStack.pop();
+					}
+
+					path.clear();
+					markedStack.clear();
+					isMarked.clear();
+					startV++;
+					if (startV >= n) {
+						nextCycle = null;
+						return;
+					}
+					isMarked.set(startV);
+					markedStack.push(startV);
+					edgeIterStack.push(g.outEdges(startV).iterator());
+				}
+			}
+
+			@Override
+			public boolean hasNext() {
+				return nextCycle != null;
+			}
+
+			@Override
+			public Path next() {
+				Assertions.Iters.hasNext(this);
+				Path ret = nextCycle;
+				advance();
+				return ret;
+			}
+		};
 	}
 
 }
