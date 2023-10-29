04873f4be59f1871612b6eed38677efe4e444e76
Barak Ugav <barakugav@gmail.com>
2023-10-29 12:27:10 +0200

    docs: package-info.java

---
+++ jgalgo-core/src/main/java/com/jgalgo/alg/package-info.java
@@ -0,0 +1,30 @@
+/*-
+ * Copyright 2023 Barak Ugav
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Algorithms for solving graph problems.
+ * <p>
+ * Most algorithms accept a {@link com.jgalgo.graph.Graph} object as input, and perform some computation on it.
+ * Algorithms in this package follow a common pattern: an interface is defined for the functionality only (e.g.
+ * {@link com.jgalgo.alg.ShortestPathSingleSource}), a result object is defined within the interface (e.g.
+ * {@link com.jgalgo.alg.ShortestPathSingleSource.Result}), a default implementation is provided via
+ * {@code newInstance()} method (e.g. {@link com.jgalgo.alg.ShortestPathSingleSource#newInstance()}), and a builder that
+ * allow more control over the algorithm is provided via {@code newBuilder()} method (e.g.
+ * {@link com.jgalgo.alg.ShortestPathSingleSource#newBuilder()}).
+ * <p>
+ * Most algorithm implementations are not expose as public API.
+ */
+package com.jgalgo.alg;
---
+++ jgalgo-core/src/main/java/com/jgalgo/graph/package-info.java
@@ -0,0 +1,50 @@
+/*-
+ * Copyright 2023 Barak Ugav
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Graphs object are the fundamental building blocks of the JGAlgo library.
+ * <p>
+ * A graph is a collection of vertices (nodes) and edges (links) between them. The edges may be directed or undirected.
+ * The {@link com.jgalgo.graph.Graph} object represent a graph, in which vertices and edges have {@code int}
+ * identifiers. Vertices can be added or removed to/from a graph, and edges can be added or removed between vertices.
+ * The {@link com.jgalgo.graph.Graph} object also provides methods to query the graph, such as the number of vertices
+ * and edges, and whether a vertex is connected to another vertex, the out-edges or in-edges of a vertex, etc.
+ * <p>
+ * Weights can be associated with the vertices or edges of a graph, and the {@link com.jgalgo.graph.Graph} object
+ * provides methods to add or remove weights to it. When a weight is added to the vertices, it is added to <i>all</i>
+ * vertices of the graph. The weights can be an {@code Object} or any other primitive such as {@code int},
+ * {@code double}, etc. When weights are added to a graph via the
+ * {@link com.jgalgo.graph.Graph#addVerticesWeights(String, Class)} or
+ * {@link com.jgalgo.graph.Graph#addEdgesWeights(String, Class)} methods, a {@link com.jgalgo.graph.Weights} object is
+ * created and return, which allow setting and getting the weights values by vertex/edge identifier. If the weights type
+ * is primitive, a specific subclass of {@link com.jgalgo.graph.Weights} such as {@link com.jgalgo.graph.WeightsInt},
+ * {@link com.jgalgo.graph.WeightsDouble}, etc. is returned. These specific primitive weights containers avoid the
+ * overhead of boxing and unboxing primitive values.
+ * <p>
+ * Each {@link com.jgalgo.graph.Graph} object expose an {@link com.jgalgo.graph.IndexGraph} via the
+ * {@link com.jgalgo.graph.Graph#indexGraph()} method. An index graph is a graph in which the identifiers of the
+ * vertices are always in the range {@code [0, n)}, where {@code n} is the number of vertices in the graph, and the
+ * identifiers of the edges are always in the range {@code [0, m)}, where {@code m} is the number of edges in the graph.
+ * Index graphs are much more efficient than regular graphs, as simple arrays are enough to store the data (internal and
+ * additional weights) associated with the vertices/edges. When a vertex or edge is removed from an index graph, some
+ * identifiers may change, as it must maintain the invariant that the identifiers are always in the range {@code [0, n)}
+ * and {@code [0, m)}. This makes the index graph less convenient to use, but much more efficient. The default graph
+ * implementation uses an index graph internally, and expose it via the {@link com.jgalgo.graph.Graph#indexGraph()}
+ * method. Good written algorithms should accept a regular graph, retrieve its index graph, perform the heavy
+ * computation on the index graph, and translate back the result to the regular graph identifiers. Index graphs should
+ * rarely be used directly by a user.
+ */
+package com.jgalgo.graph;
