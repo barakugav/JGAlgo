6a12e19219b0e22910908ae472a01c0385af7424
barakugav <barakugav@gmail.com>
2023-11-05 09:42:14 +0000

    Deploying to docs from @ barakugav/JGAlgo@0017357b1fa267a2d2f84cf786b44e96f7e47609 🚀

--- 0.2.1-snapshot/package-search-index.js
+++ 0.2.1-snapshot/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.jgalgo"},{"l":"com.jgalgo.alg"},{"l":"com.jgalgo.bench.impls"},{"l":"com.jgalgo.example"},{"l":"com.jgalgo.graph"},{"l":"com.jgalgo.io"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.jgalgo"},{"l":"com.jgalgo.alg"},{"l":"com.jgalgo.bench.impls"},{"l":"com.jgalgo.example"},{"l":"com.jgalgo.gen"},{"l":"com.jgalgo.graph"},{"l":"com.jgalgo.io"}]
