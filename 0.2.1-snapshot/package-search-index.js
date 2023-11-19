58e8a39657033bdd71570de58ebe5aa6fc936d6d
Barak Ugav <barakugav@gmail.com>
2023-11-19 20:02:22 +0200

    version 0.3.0

--- 0.2.1-snapshot/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.jgalgo"},{"l":"com.jgalgo.alg"},{"l":"com.jgalgo.bench.impls"},{"l":"com.jgalgo.example"},{"l":"com.jgalgo.gen"},{"l":"com.jgalgo.graph"},{"l":"com.jgalgo.io"}]
