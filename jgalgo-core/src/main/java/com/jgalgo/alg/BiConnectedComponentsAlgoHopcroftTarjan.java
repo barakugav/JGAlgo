00932305557ed5f9a5cde90c11f05cdf499b9cf2
Barak Ugav <barakugav@gmail.com>
2024-01-07 10:15:57 +0200

    AlgorithmBase: use interface base instead of abstract, BiConnectedComponentsAlgo

--- jgalgo-core/src/main/java/com/jgalgo/alg/BiConnectedComponentsAlgoHopcroftTarjan.java
+++ jgalgo-core/src/main/java/com/jgalgo/alg/BiConnectedComponentsAlgoHopcroftTarjan.java
@@ -15,25 +15,17 @@
  */
 package com.jgalgo.alg;
 
-import static com.jgalgo.internal.util.Range.range;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.Function;
-import java.util.stream.Collectors;
-import com.jgalgo.graph.Graphs;
 import com.jgalgo.graph.IEdgeIter;
 import com.jgalgo.graph.IndexGraph;
-import com.jgalgo.graph.IntGraph;
-import com.jgalgo.graph.IntGraphBuilder;
 import com.jgalgo.internal.util.Assertions;
 import com.jgalgo.internal.util.Bitmap;
-import com.jgalgo.internal.util.ImmutableIntArraySet;
 import it.unimi.dsi.fastutil.Pair;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntArrays;
 import it.unimi.dsi.fastutil.ints.IntList;
-import it.unimi.dsi.fastutil.ints.IntSet;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 
 /**
@@ -49,10 +41,10 @@ import it.unimi.dsi.fastutil.objects.ObjectArrayList;
  *
  * @author Barak Ugav
  */
-class BiConnectedComponentsAlgoHopcroftTarjan extends BiConnectedComponentsAlgoAbstract {
+class BiConnectedComponentsAlgoHopcroftTarjan implements BiConnectedComponentsAlgoBase {
 
 	@Override
-	BiConnectedComponentsAlgo.IResult findBiConnectedComponents(IndexGraph g) {
+	public BiConnectedComponentsAlgo.IResult findBiConnectedComponents(IndexGraph g) {
 		Assertions.Graphs.onlyUndirected(g);
 
 		final int n = g.vertices().size();
@@ -183,244 +175,7 @@ class BiConnectedComponentsAlgoHopcroftTarjan extends BiConnectedComponentsAlgoA
 			}
 		}
 
-		return new Res(g, biccs);
+		return new BiConnectedComponentsAlgos.IndexResult(g, biccs);
 	}
 
-	private static class Res implements BiConnectedComponentsAlgo.IResult {
-
-		private final IndexGraph g;
-		private final IntSet[] biccsVertices;
-		private final int[][] biccsEdgesFromAlgo;
-		private IntSet[] biccsEdges;
-		private IntSet[] vertexBiCcs;
-		private Bitmap cutVerticesBitmap;
-		private IntSet cutVertices;
-		private IntGraph blockGraph;
-
-		Res(IndexGraph g, List<Pair<int[], int[]>> biccs) {
-			this.g = Objects.requireNonNull(g);
-			final int biccsNum = biccs.size();
-
-			biccsVertices = new IntSet[biccsNum];
-			for (int biccIdx = 0; biccIdx < biccsNum; biccIdx++)
-				biccsVertices[biccIdx] = ImmutableIntArraySet.withNaiveContains(biccs.get(biccIdx).first());
-
-			biccsEdgesFromAlgo = new int[biccsNum][];
-			for (int biccIdx = 0; biccIdx < biccsNum; biccIdx++)
-				biccsEdgesFromAlgo[biccIdx] = biccs.get(biccIdx).second();
-		}
-
-		@Override
-		public IntSet getVertexBiCcs(int vertex) {
-			final int n = g.vertices().size();
-			if (vertexBiCcs == null) {
-				int[] vertexBiCcsCount = new int[n + 1];
-				for (int biccIdx = 0; biccIdx < biccsVertices.length; biccIdx++)
-					for (int v : biccsVertices[biccIdx])
-						vertexBiCcsCount[v]++;
-				int vertexBiCcsCountTotal = 0;
-				for (int v = 0; v < n; v++)
-					vertexBiCcsCountTotal += vertexBiCcsCount[v];
-
-				int[] sortedBiccs = new int[vertexBiCcsCountTotal];
-				int[] vertexOffset = vertexBiCcsCount;
-				for (int s = 0, v = 0; v < n; v++) {
-					int k = vertexOffset[v];
-					vertexOffset[v] = s;
-					s += k;
-				}
-				for (int biccIdx = 0; biccIdx < biccsVertices.length; biccIdx++)
-					for (int v : biccsVertices[biccIdx])
-						sortedBiccs[vertexOffset[v]++] = biccIdx;
-				for (int v = n; v > 0; v--)
-					vertexOffset[v] = vertexOffset[v - 1];
-				vertexOffset[0] = 0;
-
-				vertexBiCcs = new IntSet[n];
-				for (int v = 0; v < n; v++)
-					vertexBiCcs[v] =
-							ImmutableIntArraySet.withNaiveContains(sortedBiccs, vertexOffset[v], vertexOffset[v + 1]);
-			}
-			Assertions.Graphs.checkVertex(vertex, n);
-			return vertexBiCcs[vertex];
-		}
-
-		@Override
-		public int getNumberOfBiCcs() {
-			return biccsVertices.length;
-		}
-
-		@Override
-		public IntSet getBiCcVertices(int biccIdx) {
-			return biccsVertices[biccIdx];
-		}
-
-		@Override
-		public IntSet getBiCcEdges(int biccIdx) {
-			if (biccsEdges == null) {
-				final int biccsNum = getNumberOfBiCcs();
-				biccsEdges = new IntSet[biccsNum];
-
-				if (!g.isAllowParallelEdges() && !g.isAllowSelfEdges()) {
-					for (int b = 0; b < biccsNum; b++)
-						biccsEdges[b] = ImmutableIntArraySet.withNaiveContains(biccsEdgesFromAlgo[b]);
-
-				} else {
-					/*
-					 * in case parallel edges exists in the graph, we may need to manually add them to the Bi-comp edges
-					 * collection, as they will not be added by the main algorithm.
-					 */
-					int[] biccExtraEdgesCount = new int[biccsNum];
-					final int n = g.vertices().size();
-					final int m = g.edges().size();
-
-					int[] edge2bicc = new int[m];
-					Arrays.fill(edge2bicc, -1);
-					for (int b = 0; b < biccsNum; b++) {
-						for (int e : biccsEdgesFromAlgo[b]) {
-							assert edge2bicc[e] == -1;
-							edge2bicc[e] = b;
-						}
-					}
-
-					/* Search for parallel edges, which may not be included in the edges list by the main algorithm */
-					int[] extraEdgesBiccs = null;
-					if (g.isAllowParallelEdges()) {
-						int[] target2bicc = new int[n];
-						Arrays.fill(target2bicc, -1);
-						extraEdgesBiccs = new int[m];
-						Arrays.fill(extraEdgesBiccs, -1);
-						for (int u = 0; u < n; u++) {
-							for (IEdgeIter eit = g.outEdges(u).iterator(); eit.hasNext();) {
-								int e = eit.nextInt();
-								int v = eit.targetInt();
-								if (u == v)
-									continue;
-								int b = edge2bicc[e];
-								if (b != -1)
-									target2bicc[v] = b;
-							}
-							for (IEdgeIter eit = g.outEdges(u).iterator(); eit.hasNext();) {
-								int e = eit.nextInt();
-								int v = eit.targetInt();
-								if (u == v)
-									continue;
-								int b = edge2bicc[e];
-								if (b == -1) {
-									b = target2bicc[v];
-									edge2bicc[e] = b;
-									assert extraEdgesBiccs[e] == -1;
-									extraEdgesBiccs[e] = b;
-									biccExtraEdgesCount[b]++;
-								}
-							}
-							for (IEdgeIter eit = g.outEdges(u).iterator(); eit.hasNext();) {
-								eit.nextInt();
-								target2bicc[eit.targetInt()] = -1;
-							}
-						}
-					}
-
-					/* search for self edges, which are not added to any bicc */
-					if (g.isAllowSelfEdges()) {
-						assert Graphs.selfEdges(g).intStream().allMatch(e -> edge2bicc[e] == -1);
-						for (int u = 0; u < n; u++) {
-							for (IEdgeIter eit = g.outEdges(u).iterator(); eit.hasNext();) {
-								eit.nextInt();
-								int v = eit.targetInt();
-								if (u != v)
-									continue;
-								for (int b : getVertexBiCcs(u))
-									biccExtraEdgesCount[b]++;
-							}
-						}
-					}
-
-					for (int b = 0; b < biccsNum; b++) {
-						if (biccExtraEdgesCount[b] == 0)
-							continue;
-						int[] biccEdges = biccsEdgesFromAlgo[b];
-						int oldLength = biccEdges.length;
-						biccEdges = Arrays.copyOf(biccEdges, oldLength + biccExtraEdgesCount[b]);
-						biccsEdgesFromAlgo[b] = biccEdges;
-						biccExtraEdgesCount[b] = oldLength;
-					}
-
-					/* add parallel edges */
-					if (g.isAllowParallelEdges()) {
-						for (int b, e = 0; e < m; e++)
-							if ((b = extraEdgesBiccs[e]) != -1)
-								biccsEdgesFromAlgo[b][biccExtraEdgesCount[b]++] = e;
-					}
-
-					/* add self edges */
-					if (g.isAllowSelfEdges()) {
-						for (int u = 0; u < n; u++) {
-							for (IEdgeIter eit = g.outEdges(u).iterator(); eit.hasNext();) {
-								int e = eit.nextInt();
-								int v = eit.targetInt();
-								if (u != v)
-									continue;
-								for (int b : getVertexBiCcs(u))
-									biccsEdgesFromAlgo[b][biccExtraEdgesCount[b]++] = e;
-							}
-						}
-					}
-
-					for (int b = 0; b < biccsNum; b++) {
-						biccsEdges[b] = ImmutableIntArraySet.withNaiveContains(biccsEdgesFromAlgo[b]);
-						biccsEdgesFromAlgo[b] = null;
-					}
-				}
-			}
-			return biccsEdges[biccIdx];
-		}
-
-		@Override
-		public String toString() {
-			return range(getNumberOfBiCcs())
-					.mapToObj(this::getBiCcVertices)
-					.map(Object::toString)
-					.collect(Collectors.joining(", ", "[", "]"));
-		}
-
-		@Override
-		public boolean isCutVertex(int vertex) {
-			computeCutVerticesBitmap();
-			Assertions.Graphs.checkVertex(vertex, g.vertices().size());
-			return cutVerticesBitmap.get(vertex);
-		}
-
-		private void computeCutVerticesBitmap() {
-			if (cutVerticesBitmap == null)
-				cutVerticesBitmap = Bitmap.fromPredicate(g.vertices().size(), v -> getVertexBiCcs(v).size() > 1);
-		}
-
-		@Override
-		public IntSet getCutVertices() {
-			if (cutVertices == null) {
-				computeCutVerticesBitmap();
-				cutVertices = ImmutableIntArraySet.ofBitmap(cutVerticesBitmap);
-			}
-			return cutVertices;
-		}
-
-		@Override
-		public IntGraph getBlockGraph() {
-			if (blockGraph == null) {
-				final int blockNum = getNumberOfBiCcs();
-				IntGraphBuilder g = IntGraphBuilder.undirected();
-				g.ensureVertexCapacity(blockNum);
-				g.addVertices(range(blockNum));
-				for (int cutVertex : getCutVertices())
-					for (int b1 : getVertexBiCcs(cutVertex))
-						for (int b2 : getVertexBiCcs(cutVertex))
-							if (b1 != b2)
-								g.addEdge(b1, b2);
-				blockGraph = g.build();
-			}
-			return blockGraph;
-		}
-
-	}
 }
