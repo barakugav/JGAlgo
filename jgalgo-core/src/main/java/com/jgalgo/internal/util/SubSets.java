4e76989e84ee13733c996057e8d3b98b24fae198
Barak Ugav <barakugav@gmail.com>
2024-01-10 01:25:14 +0200

    Utils: sub sets iterator

---
+++ jgalgo-core/src/main/java/com/jgalgo/internal/util/SubSets.java
@@ -0,0 +1,134 @@
+/*-
+ * Copyright 2023 Barak Ugav
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jgalgo.internal.util;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import it.unimi.dsi.fastutil.ints.IntCollection;
+import it.unimi.dsi.fastutil.ints.IntImmutableList;
+import it.unimi.dsi.fastutil.ints.IntList;
+import it.unimi.dsi.fastutil.objects.ObjectImmutableList;
+
+public class SubSets {
+
+	private SubSets() {}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static <T> Iterable<List<T>> of(Collection<? extends T> set, int k) {
+		if (set instanceof IntCollection) {
+			return (Iterable) SubSets.of((IntCollection) set, k);
+		} else {
+			checkSubsetsSize(set, k);
+			return () -> new SubSetsIter<>(set, k);
+		}
+	}
+
+	public static Iterable<IntList> of(IntCollection set, int k) {
+		checkSubsetsSize(set, k);
+		return () -> new SubSetsIterInt(set, k);
+	}
+
+	private static void checkSubsetsSize(Collection<?> set, int k) {
+		if (!(0 <= k && k <= set.size()))
+			throw new IllegalArgumentException("k must be in [0, set.size()]");
+	}
+
+	private static class SubSetsIterBase {
+
+		final int[] subset;
+		final int k;
+
+		int nextDeviationIdx;
+		boolean hasNext;
+
+		SubSetsIterBase(int n, int k) {
+			this.subset = new int[k];
+			this.k = k;
+
+			for (int i = 0; i < k; i++)
+				subset[i] = n - k + i;
+			nextDeviationIdx = k;
+			hasNext = k > 0;
+		}
+
+		public boolean hasNext() {
+			return hasNext;
+		}
+
+		void advance() {
+			for (int i = 0; i < k; i++) {
+				if (subset[i] > i) {
+					nextDeviationIdx = i + 1;
+					subset[i]--;
+					while (i-- > 0)
+						subset[i] = subset[i + 1] - 1;
+					hasNext = true;
+					return;
+				}
+			}
+			hasNext = false;
+		}
+	}
+
+	private static class SubSetsIter<T> extends SubSetsIterBase implements Iterator<List<T>> {
+
+		private final T[] set;
+		private final T[] next;
+		private final List<T> nextList;
+
+		@SuppressWarnings("unchecked")
+		SubSetsIter(Collection<? extends T> set, int k) {
+			super(set.size(), k);
+			this.set = (T[]) set.toArray();
+			next = (T[]) new Object[k];
+			nextList = new ObjectImmutableList<>(next);
+		}
+
+		@Override
+		public List<T> next() {
+			Assertions.hasNext(this);
+			for (int i = 0; i < nextDeviationIdx; i++)
+				next[i] = set[subset[i]];
+			advance();
+			return nextList;
+		}
+	}
+
+	private static class SubSetsIterInt extends SubSetsIterBase implements Iterator<IntList> {
+
+		private final int[] set;
+		private final int[] next;
+		private final IntList nextList;
+
+		SubSetsIterInt(IntCollection set, int k) {
+			super(set.size(), k);
+			this.set = set.toIntArray();
+			next = new int[k];
+			nextList = new IntImmutableList(next);
+		}
+
+		@Override
+		public IntList next() {
+			Assertions.hasNext(this);
+			for (int i = 0; i < nextDeviationIdx; i++)
+				next[i] = set[subset[i]];
+			advance();
+			return nextList;
+		}
+	}
+
+}
---
+++ jgalgo-core/src/test/java/com/jgalgo/internal/util/SubSetsTest.java
@@ -0,0 +1,168 @@
+/*-
+ * Copyright 2023 Barak Ugav
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jgalgo.internal.util;
+
+import static com.jgalgo.internal.util.Range.range;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
+
+public class SubSetsTest extends TestBase {
+
+	@Test
+	public void subsets4_1() {
+		foreachBoolConfig(intSet -> {
+			Set<IntList> subsets = collectSubsetsAndAssertUnique(rangeSet(4, intSet), 1);
+			Set<IntList> expected = new ObjectOpenHashSet<>();
+			expected.add(IntList.of(0));
+			expected.add(IntList.of(1));
+			expected.add(IntList.of(2));
+			expected.add(IntList.of(3));
+			assertEquals(expected, subsets);
+		});
+	}
+
+	@Test
+	public void subsets4_2() {
+		foreachBoolConfig(intSet -> {
+			Set<IntList> subsets = collectSubsetsAndAssertUnique(rangeSet(4, intSet), 2);
+			Set<IntList> expected = new ObjectOpenHashSet<>();
+			expected.add(IntList.of(0, 1));
+			expected.add(IntList.of(0, 2));
+			expected.add(IntList.of(0, 3));
+			expected.add(IntList.of(1, 2));
+			expected.add(IntList.of(1, 3));
+			expected.add(IntList.of(2, 3));
+			assertEquals(expected, subsets);
+		});
+	}
+
+	@Test
+	public void subsets4_3() {
+		foreachBoolConfig(intSet -> {
+			Set<IntList> subsets = collectSubsetsAndAssertUnique(rangeSet(4, intSet), 3);
+			Set<IntList> expected = new ObjectOpenHashSet<>();
+			expected.add(IntList.of(0, 1, 2));
+			expected.add(IntList.of(0, 1, 3));
+			expected.add(IntList.of(0, 2, 3));
+			expected.add(IntList.of(1, 2, 3));
+			assertEquals(expected, subsets);
+		});
+	}
+
+	@Test
+	public void subsets5_2() {
+		foreachBoolConfig(intSet -> {
+			Set<IntList> subsets = collectSubsetsAndAssertUnique(rangeSet(5, intSet), 2);
+			Set<IntList> expected = new ObjectOpenHashSet<>();
+			expected.add(IntList.of(0, 1));
+			expected.add(IntList.of(0, 2));
+			expected.add(IntList.of(0, 3));
+			expected.add(IntList.of(0, 4));
+			expected.add(IntList.of(1, 2));
+			expected.add(IntList.of(1, 3));
+			expected.add(IntList.of(1, 4));
+			expected.add(IntList.of(2, 3));
+			expected.add(IntList.of(2, 4));
+			expected.add(IntList.of(3, 4));
+			assertEquals(expected, subsets);
+		});
+	}
+
+	@Test
+	public void subsets5_3() {
+		foreachBoolConfig(intSet -> {
+			Set<IntList> subsets = collectSubsetsAndAssertUnique(rangeSet(5, intSet), 3);
+			Set<IntList> expected = new ObjectOpenHashSet<>();
+			expected.add(IntList.of(0, 1, 2));
+			expected.add(IntList.of(0, 1, 3));
+			expected.add(IntList.of(0, 1, 4));
+			expected.add(IntList.of(0, 2, 3));
+			expected.add(IntList.of(0, 2, 4));
+			expected.add(IntList.of(0, 3, 4));
+			expected.add(IntList.of(1, 2, 3));
+			expected.add(IntList.of(1, 2, 4));
+			expected.add(IntList.of(1, 3, 4));
+			expected.add(IntList.of(2, 3, 4));
+			assertEquals(expected, subsets);
+		});
+	}
+
+	@Test
+	public void subsets5_4() {
+		foreachBoolConfig(intSet -> {
+			Set<IntList> subsets = collectSubsetsAndAssertUnique(rangeSet(5, intSet), 4);
+			Set<IntList> expected = new ObjectOpenHashSet<>();
+			expected.add(IntList.of(0, 1, 2, 3));
+			expected.add(IntList.of(0, 1, 2, 4));
+			expected.add(IntList.of(0, 1, 3, 4));
+			expected.add(IntList.of(0, 2, 3, 4));
+			expected.add(IntList.of(1, 2, 3, 4));
+			assertEquals(expected, subsets);
+		});
+	}
+
+	@Test
+	public void subsets5_5() {
+		foreachBoolConfig(intSet -> {
+			Set<IntList> subsets = collectSubsetsAndAssertUnique(rangeSet(5, intSet), 5);
+			Set<IntList> expected = new ObjectOpenHashSet<>();
+			expected.add(IntList.of(0, 1, 2, 3, 4));
+			assertEquals(expected, subsets);
+		});
+	}
+
+	@Test
+	public void subsets5_0() {
+		foreachBoolConfig(intSet -> {
+			Set<IntList> subsets = collectSubsetsAndAssertUnique(rangeSet(5, intSet), 0);
+			Set<IntList> expected = new ObjectOpenHashSet<>();
+			assertEquals(expected, subsets);
+		});
+	}
+
+	@Test
+	public void invalidK() {
+		assertThrows(IllegalArgumentException.class, () -> SubSets.of(rangeSet(5, true), -1));
+		assertThrows(IllegalArgumentException.class, () -> SubSets.of(rangeSet(5, true), 8));
+	}
+
+	private static Set<IntList> collectSubsetsAndAssertUnique(Collection<Integer> set, int k) {
+		Set<IntList> subsets = new ObjectOpenHashSet<>();
+		for (List<Integer> subset : SubSets.of(set, k)) {
+			boolean modified = subsets.add(new IntArrayList(subset));
+			assertTrue(modified);
+		}
+		return subsets;
+	}
+
+	private static List<Integer> rangeSet(int n, boolean intSet) {
+		List<Integer> set = range(n).asList();
+		if (!intSet)
+			set = new ArrayList<>(set);
+		return set;
+	}
+
+}
